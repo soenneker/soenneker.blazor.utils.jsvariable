4699d6ce3f4ab132f5eea136447473d290a953ed
Jake Soenneker <jake@soenneker.com>
2025-06-03 10:23:38 -0500

    misc

--- src/JsVariableInterop.cs
+++ src/JsVariableInterop.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
@@ -6,11 +5,12 @@ using Soenneker.Blazor.Utils.JsVariable.Abstract;
 using Soenneker.Extensions.Task;
 using Soenneker.Extensions.ValueTask;
 using Soenneker.Utils.AsyncSingleton;
+using Soenneker.Utils.Delay;
 
 namespace Soenneker.Blazor.Utils.JsVariable;
 
 ///<inheritdoc cref="IJsVariableInterop"/>
-public class JsVariableInterop : IJsVariableInterop
+public sealed class JsVariableInterop : IJsVariableInterop
 {
     private readonly IJSRuntime _jsRuntime;
     private readonly AsyncSingleton _scriptInitializer;
@@ -21,12 +21,12 @@ public class JsVariableInterop : IJsVariableInterop
 
         _scriptInitializer = new AsyncSingleton(async (token, _) =>
         {
-            await _jsRuntime.InvokeVoidAsync("eval", token,
-                """
-                    window.isVariableAvailable = function (variableName) {
-                        return typeof window[variableName] !== 'undefined';
-                    };
-                """).NoSync();
+            await _jsRuntime.InvokeVoidAsync("eval", token, """
+                                                                window.isVariableAvailable = function (variableName) {
+                                                                    return typeof window[variableName] !== 'undefined';
+                                                                };
+                                                            """)
+                            .NoSync();
 
             return new object();
         });
@@ -44,14 +44,12 @@ public class JsVariableInterop : IJsVariableInterop
 
         while (!await IsVariableAvailable(variableName, cancellationToken).NoSync())
         {
-            await Task.Delay(delay, cancellationToken).NoSync();
+            await DelayUtil.Delay(delay, null, cancellationToken).NoSync();
         }
     }
 
     public ValueTask DisposeAsync()
     {
-        GC.SuppressFinalize(this);
-
         return _scriptInitializer.DisposeAsync();
     }
 }
