394a9709a2e706ce49cdf58f45a3d77a099b940d
Jake Soenneker <jake@soenneker.com>
2025-12-19 11:11:54 -0600

    asyncinitializer

--- src/JsVariableInterop.cs
+++ src/JsVariableInterop.cs
@@ -2,33 +2,28 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using Soenneker.Blazor.Utils.JsVariable.Abstract;
-using Soenneker.Extensions.Task;
-using Soenneker.Extensions.ValueTask;
-using Soenneker.Utils.AsyncSingleton;
+using Soenneker.Utils.AsyncInitializers;
 using Soenneker.Utils.Delay;
 
 namespace Soenneker.Blazor.Utils.JsVariable;
 
-///<inheritdoc cref="IJsVariableInterop"/>
+/// <inheritdoc cref="IJsVariableInterop"/>
 public sealed class JsVariableInterop : IJsVariableInterop
 {
     private readonly IJSRuntime _jsRuntime;
-    private readonly AsyncSingleton _scriptInitializer;
+    private readonly AsyncInitializer _scriptInitializer;
 
     public JsVariableInterop(IJSRuntime jsRuntime)
     {
         _jsRuntime = jsRuntime;
 
-        _scriptInitializer = new AsyncSingleton(async (token, _) =>
+        _scriptInitializer = new AsyncInitializer(async token =>
         {
             await _jsRuntime.InvokeVoidAsync("eval", token, """
                                                                 window.isVariableAvailable = function (variableName) {
                                                                     return typeof window[variableName] !== 'undefined';
                                                                 };
-                                                            """)
-                            ;
-
-            return new object();
+                                                            """);
         });
     }
 
@@ -43,13 +38,8 @@ public sealed class JsVariableInterop : IJsVariableInterop
         await _scriptInitializer.Init(cancellationToken);
 
         while (!await IsVariableAvailable(variableName, cancellationToken))
-        {
             await DelayUtil.Delay(delay, null, cancellationToken);
-        }
     }
 
-    public ValueTask DisposeAsync()
-    {
-        return _scriptInitializer.DisposeAsync();
-    }
+    public ValueTask DisposeAsync() => _scriptInitializer.DisposeAsync();
 }
