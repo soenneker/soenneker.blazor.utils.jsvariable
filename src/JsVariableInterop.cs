52b3d93dcaab3538e75216a16c741b61eddcc4aa
Jake Soenneker <jake@soeneker.com>
2026-01-26 17:09:00 -0600

    CancellationScope added

--- src/JsVariableInterop.cs
+++ src/JsVariableInterop.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using Soenneker.Asyncs.Initializers;
 using Soenneker.Blazor.Utils.JsVariable.Abstract;
+using Soenneker.Extensions.CancellationTokens;
+using Soenneker.Utils.CancellationScopes;
 using Soenneker.Utils.Delay;
 
 namespace Soenneker.Blazor.Utils.JsVariable;
@@ -12,6 +14,8 @@ public sealed class JsVariableInterop : IJsVariableInterop
 {
     private readonly IJSRuntime _jsRuntime;
     private readonly AsyncInitializer _scriptInitializer;
+
+    private readonly CancellationScope _cancellationScope = new();
 
     public JsVariableInterop(IJSRuntime jsRuntime)
     {
@@ -28,19 +32,38 @@ public sealed class JsVariableInterop : IJsVariableInterop
                                                         """);
     }
 
+    private ValueTask<bool> IsVariableAvailableInternal(string variableName, CancellationToken cancellationToken)
+    {
+        return _jsRuntime.InvokeAsync<bool>("isVariableAvailable", cancellationToken, variableName);
+    }
+
     public async ValueTask<bool> IsVariableAvailable(string variableName, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken);
-        return await _jsRuntime.InvokeAsync<bool>("isVariableAvailable", cancellationToken, variableName);
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
+
+        using (source)
+        {
+            await _scriptInitializer.Init(linked);
+            return await IsVariableAvailableInternal(variableName, linked);
+        }
     }
 
     public async ValueTask WaitForVariable(string variableName, int delay = 100, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken);
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
 
-        while (!await IsVariableAvailable(variableName, cancellationToken))
-            await DelayUtil.Delay(delay, null, cancellationToken);
+        using (source)
+        {
+            await _scriptInitializer.Init(linked);
+
+            while (!await IsVariableAvailableInternal(variableName, linked))
+                await DelayUtil.Delay(delay, null, linked);
+        }
     }
 
-    public ValueTask DisposeAsync() => _scriptInitializer.DisposeAsync();
+    public async ValueTask DisposeAsync()
+    {
+        await _scriptInitializer.DisposeAsync();
+        await _cancellationScope.DisposeAsync();
+    }
 }
