cbbf1a591493a1751a543895de656819d690aff8
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:23 -0500

    TUnit conversion

--- test/Soenneker.Blazor.Utils.JsVariable.Tests/JsVariableInteropTests.cs
+++ test/Soenneker.Blazor.Utils.JsVariable.Tests/JsVariableInteropTests.cs
@@ -1,21 +1,19 @@
 using Soenneker.Blazor.Utils.JsVariable.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
-
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Blazor.Utils.JsVariable.Tests;
 
-[Collection("Collection")]
-public class JsVariableInteropTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class JsVariableInteropTests : HostedUnitTest
 {
     private readonly IJsVariableInterop _util;
 
-    public JsVariableInteropTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public JsVariableInteropTests(Host host) : base(host)
     {
         _util = Resolve<IJsVariableInterop>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
