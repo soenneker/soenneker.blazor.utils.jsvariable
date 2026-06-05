11691524f13be1b842dbae5be9aa84032420f430
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:06 -0500

    xml commenting

--- src/Soenneker.Blazor.Utils.JsVariable/JsVariableInterop.cs
+++ src/Soenneker.Blazor.Utils.JsVariable/JsVariableInterop.cs
@@ -79,6 +79,10 @@ public sealed class JsVariableInterop : IJsVariableInterop
         }
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async ValueTask DisposeAsync()
     {
         await _moduleImportUtil.DisposeContentModule(_modulePath)
