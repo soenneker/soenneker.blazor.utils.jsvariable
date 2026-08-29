b0c07ebd1b5a3d437c837b2000ad00a7f0b7f365
soenneker <jake@soenneker.com>
2026-08-29 14:16:53 -0500

    Improve public API XML documentation

--- src/Soenneker.Blazor.Utils.JsVariable/Registrars/JsVariableInteropRegistrar.cs
+++ src/Soenneker.Blazor.Utils.JsVariable/Registrars/JsVariableInteropRegistrar.cs
@@ -13,6 +13,8 @@ public static class JsVariableInteropRegistrar
     /// <summary>
     /// Adds <see cref="IJsVariableInterop"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddJsVariableInteropAsScoped(this IServiceCollection services)
     {
         services.AddModuleImportUtilAsScoped().TryAddScoped<IJsVariableInterop, JsVariableInterop>();
